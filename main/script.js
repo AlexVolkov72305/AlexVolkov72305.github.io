f2bef83be102095b9f0f44be6f24e792df036b49
AlexVolkov72305 <35425272+AlexVolkov72305@users.noreply.github.com>
2018-05-27 19:08:00 +0300

    Main1.0
    Main 1.0

--- main/script.js
+++ main/script.js
@@ -1,22 +1,82 @@
 
-$(function() {
-    $('.menu-head ul li').hover(
-        /* Функция исполняется, когда мышь входит в область меню */
-        function mouseOver() {
-            clearTimeout($.data(this, 'timer')); /* если мы успели вернуться мышкой на меню, до того как прошла задержка на закрытие, то таймер обнуляется и сворачивание меню не произойдет*/
-            /* если использовать stop(true, FALSE), то скачка на конец анимации не будет (false стоит по умолчанию) */
-            $(this).find('> ul').stop(false).slideDown(1000); /* анимация разворачивания меню */
-        },
- 
-        /* Функция исполняется, когда мышь покидает область меню */
-        function mouseOut() {
-            var $this = $(this); /* запоминаем окружение, т.к. в Таймере мы до него не достучимся*/
-            $.data(this, 'timer', setTimeout( /* Устанавливаем таймер, чтобы закрытие меню не начиналось сразу после того как мышь покинет li */
-                    function () {
-                         $this.find('> ul').stop(false).slideUp(1000); /* анимация сворачивания меню */
-                    },
-                    200) /* задержка закрытия меню */
-            );
-        });
+var lnk1 = document.getElementsByClassName("lnk1-collps");
+var lnk2 = document.getElementsByClassName("lnk2-collps");
+var lnk3 = document.getElementsByClassName("lnk3-collps");
+var i;
+
+for (i = 0; i < lnk1.length; i++) {
+	lnk1[i].onclick = function () {
+		
+		$('.open_btn1').fadeIn();
+		$('.btn1_open').fadeOut();
+		$('.btn1_close').fadeIn();
+	}
+}
+
+for (i = 0; i < lnk2.length; i++) {
+	lnk2[i].onclick = function () {
+		
+		$('.open_btn2').fadeIn();
+		$('.btn2_open').fadeOut();
+		$('.btn2_close').fadeIn();
+	}
+}
+
+for (i = 0; i < lnk3.length; i++) {
+	lnk3[i].onclick = function () {
+		
+		$('.open_btn3').fadeIn();
+		$('.btn3_open').fadeOut();
+		$('.btn3_close').fadeIn();
+	}
+}
+
+var used;
+
+$(".lnk1-collps").on("click", function(e){
+    if( used !== this) {
+			used = this; 
+			e.preventDefault();
+		}
 });
 
+$(".lnk2-collps").on("click", function(e){
+    if( used !== this) {
+			used = this; 
+			e.preventDefault();
+		}
+});
+
+$(".lnk3-collps").on("click", function(e){
+    if( used !== this) {
+			used = this; 
+			e.preventDefault();
+		}
+});
+
+$(document).ready(function(a){
+	$('.btn1_close').on('click', function(event) {
+		event.preventDefault();
+		$('.open_btn1').fadeOut();
+		$('.btn1_close').fadeOut();
+		$('.btn1_open').fadeIn();
+	});
+});
+
+$(document).ready(function(b){
+	$('.btn2_close').on('click', function(event) {
+		event.preventDefault();
+		$('.open_btn2').fadeOut();
+		$('.btn2_close').fadeOut();
+		$('.btn2_open').fadeIn();
+	});
+});
+
+$(document).ready(function(c){
+	$('.btn3_close').on('click', function(event) {
+		event.preventDefault();
+		$('.open_btn3').fadeOut();
+		$('.btn3_close').fadeOut();
+		$('.btn3_open').fadeIn();
+	});
+});
