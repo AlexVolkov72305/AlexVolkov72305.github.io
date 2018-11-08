3fad0d36891bfd012b3e873f33b40e157035dd0b
AlexVolkov72305 <35425272+AlexVolkov72305@users.noreply.github.com>
2018-11-08 19:53:20 +0300

    Samovar
    Samovar

--- Samovar/production/js/app.js
+++ Samovar/production/js/app.js
@@ -6630,223 +6630,77 @@ module.exports = g;
 "use strict";
 
 
-//Таб в блоке Наши работы
+/* Пишем свой калькулятор на сайт */
 
-var tab;
-var tabContent;
+$(function () {
+	var selectBox = $('.select_box'),
+	    tan = $('.checkbox1'),
+	    powerСontrol = $('.checkbox2'),
+	    plumbingKit = $('.checkbox3'),
+	    delivery = $('.checkbox4'),
+	    price = $('#price'),
+	    priceSelect = $('#price_select'),
+	    summ = 11999,
+	    design = 0,
+	    design2 = 0,
+	    design3 = 0,
+	    design4 = 0;
 
-window.onload = function () {
-				tabContent = document.getElementsByClassName('tabContent');
-				tab = document.getElementsByClassName('tab');
-				hideTabsContent(1);
-};
-function hideTabsContent(a) {
-				for (var i = a; i < tabContent.length; i++) {
-								tabContent[i].classList.remove('show');
-								tabContent[i].classList.add('hide');
-								tab[i].classList.remove('whiteborder');
-				}
-}
+	selectBox.change(function (event) {
+		var summ = +selectBox.val() * 400 + design + design2 + design3 + design4,
+		    selectBoxPrice = +selectBox.val() * 400;
+		priceSelect.text(selectBoxPrice + ' P');
+		price.text(summ + ' P');
+	});
 
-document.getElementById('tabs').onclick = function (event) {
-				var target = event.target;
-				if (target.className == 'tab') {
-								for (var i = 0; i < tab.length; i++) {
-												if (target == tab[i]) {
-																showTabsContent(i);
-																break;
-												}
-								}
-				}
-};
-function showTabsContent(b) {
-				if (tabContent[b].classList.contains('hide')) {
-								hideTabsContent(0);
-								tab[b].classList.add('whiteborder');
-								tabContent[b].classList.remove('hide');
-								tabContent[b].classList.add('show');
-				}
-}
+	tan.change(function (event) {
+		if ($(this).is(':checked')) {
+			design = 2000;
+			summ = +selectBox.val() * 400 + design + design2 + design3 + design4;
+			price.text(summ + ' P');
+		} else {
+			design = 0;
+			summ = +selectBox.val() * 400 + design + design2 + design3 + design4;
+			price.text(summ + ' P');
+		}
+	});
 
-(function ($) {
-				$(function () {
+	powerСontrol.change(function (event) {
+		if ($(this).is(':checked')) {
+			design2 = 2000;
+			summ = +selectBox.val() * 400 + design + design2 + design3 + design4;
+			price.text(summ + ' P');
+		} else {
+			design2 = 0;
+			summ = +selectBox.val() * 400 + design + design2 + design3 + design4;
+			price.text(summ + ' P');
+		}
+	});
 
-								$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
-												$(this).addClass('active').siblings().removeClass('active').closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-								});
-				});
-})(jQuery);
+	plumbingKit.change(function (event) {
+		if ($(this).is(':checked')) {
+			design3 = 0;
+			summ = +selectBox.val() * 400 + design + design2 + design3 + design4;
+			price.text(summ + ' P');
+		} else {
+			design3 = 0;
+			summ = +selectBox.val() * 400 + design + design2 + design3 + design4;
+			price.text(summ + ' P');
+		}
+	});
+	delivery.change(function (event) {
+		if ($(this).is(':checked')) {
+			design4 = 0;
+			summ = +selectBox.val() * 400 + design + design2 + design3 + design4;
+			price.text(summ + ' P');
+		} else {
+			design4 = 0;
+			summ = +selectBox.val() * 400 + design + design2 + design3 + design4;
+			price.text(summ + ' P');
+		}
+	});
 
-$(".btn").on("click", function () {
-				$(".main").css("right", "-20px");
-				$(".main").css("position", "absolute");
-				$(".main").css("transition", "all 0.4s");
-				$(".close").css("display", "block");
-				$("#on_line").css("color", "#544b34");
-				$("#on_line span").css("color", "#544b34");
-				$(".btn").css("display", "none");
-				$(".slick-next").css("display", "none");
-				$(".slick-prev").css("display", "none");
-});
-
-$(".close").on("click", function () {
-				$(".main").css("right", "-100%");
-				$(".main").css("position", "fixed");
-				$(this).addClass('active');
-				$(".main").css("transition", "all 0.4s");
-				$(".close").css("display", "none");
-				$(".btn").css("display", "block");
-				$("#on_line").css("color", "#ffffff");
-				$("#on_line span").css("color", "#ffffff");
-				$(".slick-next").css("display", "block");
-				$(".slick-prev").css("display", "block");
-});
-
-//Слайдер секции Header
-
-$(document).ready(function () {
-				$('.header_slider').slick({
-								slideshowSpeed: 1000,
-								animation: "slide",
-								controlNav: false
-				});
-});
-
-//Слайдер секции Benefits
-
-$(document).ready(function () {
-				$('.benefits_slider').slick({
-								slideshowSpeed: 1000,
-								animation: "slide",
-								prevArrow: '<button class="arr-prev" type="button"></button>',
-								nextArrow: '<button class="arr-next" type="button"></button>',
-								controlNav: false
-				});
-});
-
-$(document).ready(function () {
-				$('.about_slider').slick({
-								slideshowSpeed: 1000,
-								animation: "slide",
-								prevArrow: '<button class="arr-prev" type="button"></button>',
-								nextArrow: '<button class="arr-next" type="button"></button>',
-								controlNav: false
-				});
-});
-
-$(document).ready(function () {
-				$('.photo_slider').slick({
-								infinite: true,
-								slidesToShow: 3,
-								prevArrow: '<button class="arr-prev" type="button"></button>',
-								nextArrow: '<button class="arr-next" type="button"></button>',
-								slidesToScroll: 1,
-								responsive: [{
-												breakpoint: 992,
-												settings: {
-																slidesToShow: 3,
-																slidesToScroll: 3,
-																infinite: true
-												}
-								}, {
-												breakpoint: 768,
-												settings: {
-																slidesToShow: 2,
-																slidesToScroll: 1,
-																prevArrow: '<button class="arr-prev" type="button"></button>',
-																nextArrow: '<button class="arr-next" type="button"></button>'
-												}
-								}, {
-												breakpoint: 576,
-												settings: {
-																slidesToShow: 1,
-																slidesToScroll: 1,
-																prevArrow: '<button class="arr-prev" type="button"></button>',
-																nextArrow: '<button class="arr-next" type="button"></button>'
-												}
-								}]
-				});
-});
-
-$(document).ready(function () {
-				$('.masters_slider').slick({
-								infinite: true,
-								slidesToShow: 3,
-								prevArrow: '<button class="arr-prev2" type="button"></button>',
-								nextArrow: '<button class="arr-next2" type="button"></button>',
-								slidesToScroll: 1,
-								responsive: [{
-												breakpoint: 992,
-												settings: {
-																slidesToShow: 3,
-																slidesToScroll: 3,
-																infinite: true
-												}
-								}, {
-												breakpoint: 768,
-												settings: {
-																slidesToShow: 2,
-																slidesToScroll: 1
-												}
-								}, {
-												breakpoint: 576,
-												settings: {
-																slidesToShow: 1,
-																slidesToScroll: 1,
-																prevArrow: '<button class="arr-prev" type="button"></button>',
-																nextArrow: '<button class="arr-next" type="button"></button>'
-												}
-								}]
-				});
-});
-
-$(document).ready(function () {
-				$('.prise_slider').slick({
-								infinite: true,
-								slidesToShow: 3,
-								prevArrow: '<button class="arr-prev2" type="button"></button>',
-								nextArrow: '<button class="arr-next2" type="button"></button>',
-								slidesToScroll: 1,
-								responsive: [{
-												breakpoint: 992,
-												settings: {
-																slidesToShow: 3,
-																slidesToScroll: 1,
-																infinite: true
-												}
-								}, {
-												breakpoint: 768,
-												settings: {
-																slidesToShow: 1,
-																slidesToScroll: 1,
-																prevArrow: '<button class="arr-prev2" type="button"></button>',
-																nextArrow: '<button class="arr-next2" type="button"></button>'
-												}
-								}, {
-												breakpoint: 576,
-												settings: {
-																slidesToShow: 1,
-																slidesToScroll: 1,
-																prevArrow: '<button class="arr-prev" type="button"></button>',
-																nextArrow: '<button class="arr-next" type="button"></button>'
-												}
-								}]
-				});
-				$('tab_boutique').on('shown.bs.tab', function (e) {
-								$('.prise_slider').slick('reinit');
-				});
-});
-
-$(window).scroll(function () {
-				if ($(window).scrollTop() > 500) {
-								$('.header_nav').addClass('scroll');
-								$(".header_nav").css("opacity", "0.8");
-								$(".header_nav").css("transition ", "3s");
-				} else {
-								$('.header_nav').removeClass('scroll');
-								$(".header_nav").css("opacity", "0.5");
-								$(".header_nav").css("transition ", "3s");
-				}
+	price.text(summ + ' P');
 });
 
 /***/ })
