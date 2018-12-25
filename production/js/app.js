e636f4ed001aa5856d9d8443d52ecaee2109773f
AlexVolkov72305 <35425272+AlexVolkov72305@users.noreply.github.com>
2018-12-25 22:03:02 +0300

    Home_1
    Home_1

--- production/js/app.js
+++ production/js/app.js
@@ -6630,176 +6630,34 @@ module.exports = g;
 "use strict";
 
 
-$(".btn").on("click", function () {
-				$(".main").css("right", "-20px");
-				$(".main").css("position", "fixed");
-				$(".main").css("transition", "all 0.4s");
-				$(".close").css("display", "block");
-				$("#on_line").css("color", "#544b34");
-				$("#on_line span").css("color", "#544b34");
-				$(".btn").css("display", "none");
-				$(".slick-next").css("display", "none");
-				$(".slick-prev").css("display", "none");
+/* Делаем адаптивное меню на jQuery */
+
+$(function () {
+	var menuLink = $('.menu-link');
+	var menu = $('.main_mobile');
+	var close = $('.close-btn');
+	var navLink = $('.main_mobile li a');
+
+	menuLink.click(function () {
+		menu.toggleClass('active-menu');
+		$(".menu-link").css("display", "none");
+		$(".close-btn").css("display", "block");
+	});
+	close.click(function () {
+		menu.toggleClass('active-menu');
+		$(".menu-link").css("display", "block");
+		$(".close-btn").css("display", "none");
+	});
 });
 
-$(".close").on("click", function () {
-				$(".main").css("right", "-100%");
-				$(".main").css("position", "fixed");
-				$(this).addClass('active');
-				$(".main").css("transition", "all 0.4s");
-				$(".close").css("display", "none");
-				$(".btn").css("display", "block");
-				$("#on_line").css("color", "#ffffff");
-				$("#on_line span").css("color", "#ffffff");
-				$(".slick-next").css("display", "block");
-				$(".slick-prev").css("display", "block");
-});
-
-//Слайдер секции Header
-
-$(document).ready(function () {
-				$('.header_slider').slick({
-								slideshowSpeed: 1000,
-								animation: "slide",
-								controlNav: false
-				});
-});
-
-//Слайдер секции Benefits
-
-$(document).ready(function () {
-				$('.benefits_slider').slick({
-								slideshowSpeed: 1000,
-								animation: "slide",
-								prevArrow: '<button class="arr-prev" type="button"></button>',
-								nextArrow: '<button class="arr-next" type="button"></button>',
-								controlNav: false
-				});
-});
-
-$(document).ready(function () {
-				$('.about_slider').slick({
-								slideshowSpeed: 1000,
-								animation: "slide",
-								prevArrow: '<button class="arr-prev" type="button"></button>',
-								nextArrow: '<button class="arr-next" type="button"></button>',
-								controlNav: false
-				});
-});
-
-$(document).ready(function () {
-				$('.photo_slider').slick({
-								infinite: true,
-								slidesToShow: 3,
-								prevArrow: '<button class="arr-prev" type="button"></button>',
-								nextArrow: '<button class="arr-next" type="button"></button>',
-								slidesToScroll: 1,
-								responsive: [{
-												breakpoint: 992,
-												settings: {
-																slidesToShow: 3,
-																slidesToScroll: 3,
-																infinite: true
-												}
-								}, {
-												breakpoint: 768,
-												settings: {
-																slidesToShow: 2,
-																slidesToScroll: 1,
-																prevArrow: '<button class="arr-prev" type="button"></button>',
-																nextArrow: '<button class="arr-next" type="button"></button>'
-												}
-								}, {
-												breakpoint: 576,
-												settings: {
-																slidesToShow: 1,
-																slidesToScroll: 1,
-																prevArrow: '<button class="arr-prev" type="button"></button>',
-																nextArrow: '<button class="arr-next" type="button"></button>'
-												}
-								}]
-				});
-});
-
-$(document).ready(function () {
-				$('.masters_slider').slick({
-								infinite: true,
-								slidesToShow: 3,
-								prevArrow: '<button class="arr-prev2" type="button"></button>',
-								nextArrow: '<button class="arr-next2" type="button"></button>',
-								slidesToScroll: 1,
-								responsive: [{
-												breakpoint: 992,
-												settings: {
-																slidesToShow: 3,
-																slidesToScroll: 3,
-																infinite: true
-												}
-								}, {
-												breakpoint: 768,
-												settings: {
-																slidesToShow: 2,
-																slidesToScroll: 1
-												}
-								}, {
-												breakpoint: 576,
-												settings: {
-																slidesToShow: 1,
-																slidesToScroll: 1,
-																prevArrow: '<button class="arr-prev" type="button"></button>',
-																nextArrow: '<button class="arr-next" type="button"></button>'
-												}
-								}]
-				});
-});
-
-$(document).ready(function () {
-				$('.prise_slider').slick({
-								infinite: true,
-								slidesToShow: 3,
-								prevArrow: '<button class="arr-prev2" type="button"></button>',
-								nextArrow: '<button class="arr-next2" type="button"></button>',
-								slidesToScroll: 1,
-								responsive: [{
-												breakpoint: 992,
-												settings: {
-																slidesToShow: 3,
-																slidesToScroll: 1,
-																infinite: true
-												}
-								}, {
-												breakpoint: 768,
-												settings: {
-																slidesToShow: 1,
-																slidesToScroll: 1,
-																prevArrow: '<button class="arr-prev2" type="button"></button>',
-																nextArrow: '<button class="arr-next2" type="button"></button>'
-												}
-								}, {
-												breakpoint: 576,
-												settings: {
-																slidesToShow: 1,
-																slidesToScroll: 1,
-																prevArrow: '<button class="arr-prev" type="button"></button>',
-																nextArrow: '<button class="arr-next" type="button"></button>'
-												}
-								}]
-				});
-				$('tab_boutique').on('shown.bs.tab', function (e) {
-								$('.prise_slider').slick('reinit');
-				});
-});
-
-$(window).scroll(function () {
-				if ($(window).scrollTop() > 500) {
-								$('.header_nav').addClass('scroll');
-								$(".header_nav").css("opacity", "0.8");
-								$(".header_nav").css("transition ", "3s");
-				} else {
-								$('.header_nav').removeClass('scroll');
-								$(".header_nav").css("opacity", "0.5");
-								$(".header_nav").css("transition ", "3s");
-				}
+jQuery(document).ready(function ($) {
+	$('.slider_head').slick({
+		slideshowSpeed: 1000,
+		animation: "slide",
+		prevArrow: $('.prev'),
+		nextArrow: $('.next'),
+		controlNav: false
+	});
 });
 
 /***/ })
